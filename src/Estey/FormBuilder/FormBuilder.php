287d7c19370b7864f872d5033c56cdfd6b3e1f78
Brad Estey <brad.estey@gmail.com>
2014-10-17 22:46:14 -0400

    Revise FormBuilder to match Laravel 4.1 FormBuilder.

--- src/Estey/FormBuilder/FormBuilder.php
+++ src/Estey/FormBuilder/FormBuilder.php
@@ -82,15 +82,16 @@ class FormBuilder extends \Illuminate\Html\FormBuilder {
 	 * @param  string  $name
 	 * @param  string  $selected
 	 * @param  array   $options
+	 * @param  string  $format
 	 * @return string
 	 */
-	public function selectMonth($name, $selected = null, $options = array())
+	public function selectMonth($name, $selected = null, $options = array(), $format = '%B')
 	{
 		$months = array();
 
 		foreach (range(1, 12) as $month)
 		{
-			$months[$month] = strftime('%B', mktime(0, 0, 0, $month, 1));
+			$months[$month] = strftime($format, mktime(0, 0, 0, $month, 1));
 			
 			$key = 'datetime.'.strtolower($months[$month]);
 			if ($this->translator->has($key))
